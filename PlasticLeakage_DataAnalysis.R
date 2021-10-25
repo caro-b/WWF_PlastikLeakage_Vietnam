5706ddcb50b836e892c00d783a9eca4f5bbf20f2
caro-b <caroline.anton@web.de>
2021-10-25 17:17:13 +0200

    heavy rain & wind

--- PlasticLeakage_DataAnalysis.R
+++ PlasticLeakage_DataAnalysis.R
@@ -11,7 +11,7 @@
 #### I. SETUP ####
 
 # install required packages (if not installed yet)
-packagelist <- c("dplyr","tidyverse")
+packagelist <- c("dplyr","raster","sp","tidyverse")
 new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -78,13 +78,18 @@ prcp_meteostat <- rename(prcp_meteostat, date = time)
 
 ## a) Meteostat
 ## rename columns
-wind_meteostat <- rename(wind_meteostat, date = time)
+wind_meteostat <- rename(wind_meteostat, datetime = time)
+
+## no NAs or exact duplicates  (already checked in python download script)
+
+## number of duplicates in station, datetime & wind speed
+sum(duplicated(wind_meteostat[,1:3])) #0
 
 
 
 #### IV. DATA MERGING ####
 
-#### 1. Precipitation (daily)
+#### Precipitation (daily)
 
 # first convert to same data type
 typeof(prcp_noaa$station) # character
@@ -92,7 +97,7 @@ typeof(prcp_meteostat$station) # double
 prcp_meteostat$station <- as.character(prcp_meteostat$station)
 
 ## merge data by station id
-prcp_merge <- rbind(prcp_noaa %>% select(station, date, prcp), prcp_meteostat %>% select(station, date, prcp))
+prcp_merge <- rbind(prcp_noaa[,c("station", "date", "prcp")], prcp_meteostat[,c("station", "date", "prcp")])
 
 ## remove exact duplicates
 prcp_merge <- distinct(prcp_merge)
@@ -103,15 +108,69 @@ sum(duplicated(prcp_merge[,1:2])) #4443
 ## for multiple prcp values at same station & date - take average
 prcp_merge <- prcp_merge %>% group_by(station, date) %>% summarize(prcp_mean=mean(prcp))
 
+
+
+#### IV. DATA ANALYSIS ####
+
+#### Heavy Rain days per station (>=100 mm rain per day)
+heavyrain_stations <- prcp_merge %>% group_by(station) %>% summarise(heavyraindays = sum(prcp_mean >= 100))
+
+#### Heavy Wind days per station (>39km/h)
+heavywind_stations <- wind_meteostat %>% group_by(station) %>% summarise(heavywindhours = sum(wspd >= 39))
+
+  
+
+## add coordinates to stations
+
 ## station location data - from meteostat data (as noaa is only one of its sources)
 station_data <- unique(prcp_meteostat[,c(1,4:6)])
 
+
+## 1. Precipitation
+
 ## join extra station information to prcp data
-prcp_data <- left_join(prcp_merge, station_data, by ="station")
+prcp_data <- left_join(heavyrain_stations, station_data, by ="station")
 
 
-## No. of prcp stations
-length(unique(prcp_merge$station)) #18
+## 2. Wind
+
+# first convert to same data type
+typeof(heavywind_stations$station) # double
+typeof(station_data$station) # character
+
+# change to character data type
+heavywind_stations$station <- as.character(heavywind_stations$station)
+
+wspd_data <- left_join(heavywind_stations, station_data, by ="station")
 
 
+  
+
+
+#### V. create polygons from coordinates ####
+
+#### 1. Precipitation (daily)
+
+## create spatialpoints from lat, long coordinates
+prcp_stations <- SpatialPointsDataFrame(coords = c(prcp_data[,c("longitude","latitude")]),
+                                    proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
+                                    data = prcp_data)
+plot(prcp_stations)
+
+## export as shapefile
+shapefile(x = prcp_stations, file = paste(dir, "/precipitation_stations_VN.shp", sep = ""))
+
+
+#### 2. Wind (hourly)
+
+## create spatialpoints from lat, long coordinates
+wspd_stations <- SpatialPointsDataFrame(coords = c(wspd_data[,c("longitude","latitude")]),
+                                        proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
+                                        data = wspd_data)
+plot(wspd_stations)
+
+shapefile(x = wspd_stations, file = paste(dir, "/windspeed_stations_VN.shp", sep = ""))
+
+
+#### TODO: combine prcp & wspd into one shapefile?? ####
 
