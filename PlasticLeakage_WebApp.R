2119a9d28d30355ed4681d3afb065da504ad5ac6
caro-b <caroline.anton@web.de>
2021-12-23 15:06:12 +0100

    webapp update after landfill drawing

--- PlasticLeakage_WebApp.R
+++ PlasticLeakage_WebApp.R
@@ -9,7 +9,7 @@
 ## Develop an interactive web app for users & stakeholders to get an overview over the risk assessment & the data included
 
 # install required packages (if not installed yet)
-packagelist <- c("dplyr","ggplot2","leafem","leaflet","raster","RColorBrewer","readr","rgdal","sf","shiny","sp","stars","tidyverse")
+packagelist <- c("class", "dplyr","ggplot2","leafem","leaflet","leaflet.extras","raster","RColorBrewer","readr","rgdal","sf","shiny","sp","stars","tidyverse")
 new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -22,16 +22,15 @@ dir <- 'C:/Users/carob/Documents/WWF_PlastikLeakage_Vietnam/data'
 
 #### 0. Import Data ####
 
-## import landfills clusters spatial data
-landfills_sf <- st_read(paste(dir,"/landfill_clusters.gpkg", sep= ""))
+## import landfills clusters spatial data (point data)
+landfills_clusters <- readOGR(paste(dir,"/landfill_clusters.gpkg", sep= ""))
+landfills_clusters_sf <- st_read(paste(dir,"/landfill_clusters.gpkg", sep= ""))
 
 ### import landfill polygons
-landfills <- readOGR(paste(dir, "/landfills/OpenLandfills_Vietnam.shp", sep = ""), use_iconv = T, encoding = "UTF-8")
+landfills_polygons <- readOGR(paste(dir, "/landfills/OpenLandfills_Vietnam.shp", sep = ""), use_iconv = T, encoding = "UTF-8")
 
 ## import shapefile of vietnam
 vietnam <- readOGR(paste(dir, "/vietnam/vietnam.shp", sep = ""))
-
-## import data layers of web app
 
 
 
@@ -43,100 +42,21 @@ vietnam <- readOGR(paste(dir, "/vietnam/vietnam.shp", sep = ""))
 # create color palette
 cof <- colorFactor(c("green","blue","red"), domain=c("1","2","3"))
 
-map <- leaflet(landfills_sf) %>%
-  #addTiles() %>%  # Add default OpenStreetMap map tiles
+map <- leaflet(landfills_clusters_sf) %>%
   addProviderTiles(providers$Esri.WorldImagery) %>% 
-  #addProviderTiles(providers$CartoDB.DarkMatter) %>% 
   setView(lng = 105.48, lat = 15.54, zoom = 6) %>%
   addMiniMap %>%
   addPolygons(data = vietnam, fill = F, weight = 2, color = "#FFFFCC", group = "Outline") %>%
-  addPolygons(data = landfills, fill = F, weight = 2, color = "black") %>%
-  addCircleMarkers(data = landfills_sf, color = ~cof(km_cluster_unstand), radius = sqrt(landfills_sf$area_ha)*2, 
+  addPolygons(data = landfills_polygons, fill = F, weight = 2, color = "#FFFFCC") %>%
+  addCircleMarkers(data = landfills_clusters_sf, color = ~cof(km_cluster_unstand), radius = sqrt(landfills_clusters_sf$area_ha)*2, 
                    fillOpacity = 0.5, label = ~name, group = "Risk") %>%
-  addLegend("bottomleft", colors= c("red","blue","green"), labels=c("high", "medium", "low"), title="Leakage Risk") 
+  addLegend("bottomleft", colors= c("red","blue","green"), labels=c("high", "medium", "low"), title="Leakage Risk")
 map
 
 
 
-#### add raster images as interactive layers
-# may take some time to load due to big raster data sets
-# for large rasters use stars package
-
-## DEM
-dem <- read_stars(paste(dir, "/dem/dem_slope.tif", sep = ""))
-
-st_is_longlat(dem)
-
-fl <- tempfile(fileext = ".tif")
-write_stars(dem, dsn = fl)
-
-# color palette
-pal_dem <- RColorBrewer::brewer.pal(9, "Greys")
-
-# define own color palette
-pal_dem2 <- colorNumeric(palette = c('#ffffff', '#808080', '#000000'), domain = c(0,80), na.color = "transparent")
-
-
-## JRC Water
-jrc <- read_stars("C:/Users/carob/Documents/WWF_PlastikLeakage_Vietnam/data/JRC_GlobalSurfaceWater_Vietnam_30.tif")
-
-st_is_longlat(jrc)
-
-## needs to be down-sampled else raster to big for R to handle
-jrc_ds <- stars:::st_downsample(jrc, n = 10)
-dim(jrc_ds)
-
-file <- tempfile(fileext = ".tif")
-write_stars(jrc_ds, dsn = file)
-
-# remove 0 values for plotting
-jrc_ds[jrc_ds == 0] <- NA
-write_stars(jrc_ds, dsn = file)
-
-# define own color palette
-pal <- RColorBrewer::brewer.pal(9, "Blues")
-
-# define own color palette
-pal_jrc <- colorNumeric(palette= c("#FFFFCC", "#41B6C4", "#0C2C84"), domain = c(0,100), na.color = "transparent")
-
-
-map_raster <- leaflet(options = leafletOptions(noWrap = T)) %>%
-  setView(lng = 105.48, lat = 15.54, zoom = 5) %>%
-  # Base groups
-  #addTiles(group = "OSM") %>% # Add OpenStreetMap map tiles
-  #addProviderTiles("OpenStreetMap", group = "OSM") %>% 
-  #addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark") %>%
-  addProviderTiles(providers$Esri.WorldImagery) %>% 
-  addMiniMap %>% 
-  # Add raster images (as overlay groups)
-  # addGeoRaster for stars & star_proxy objects
-  leafem:::addGeotiff(file = fl, group = "DEM", colorOptions = leafem:::colorOptions( 
-    palette = pal_dem, na.color = "transparent")) %>%
-  leafem:::addGeotiff(file = file, group = "Water", colorOptions = leafem:::colorOptions(
-    palette = pal, na.color = "transparent")) %>%
-  # Overlay groups
-  addPolygons(data = vietnam, fill = F, weight = 2, color = "#FFFFCC", group = "Outline") %>%
-  addPolygons(data = landfills, fill = F, weight = 2, color = "black", group = "Landfills") %>%
-  addCircleMarkers(data = landfills_sf, color = ~cof(km_cluster_unstand), radius = sqrt(landfills_sf$area_ha)*2, 
-                   fillOpacity = 0.5, label = ~name, group = "Risk") %>%
-  addLegend("bottomleft", colors = c("red","blue","green"), labels = c("high", "medium", "low"), 
-            title ="Leakage Risk", group = "Risk") %>%
-  addLegend(pal = pal_dem2, values = c(0,80), bins = c(0,80), labFormat = labelFormat(suffix = "°"),
-            title = "Slope", group = "DEM") %>%
-  addLegend(pal = pal_jrc, values = c(0,100), bins = c(0,100), labFormat = labelFormat(suffix = "%"),
-            title = "Water Occurrence", group = "Water") %>%
-  # Layers control
-  addLayersControl(
-    overlayGroups = c("DEM", "Water"),
-    options = layersControlOptions(collapsed = F)
-  )
-map_raster
-
-
-
 #### Interactive Map ####
-
-ui_inter <- navbarPage("Superzip", id="nav",
+ui_inter <- fluidPage("Classification the Plastic Leakage Risk of Landfills in Vietnam", id="nav",
            
   tabPanel("Interactive Map",
     div(class="outer",
@@ -150,13 +70,7 @@ ui_inter <- navbarPage("Superzip", id="nav",
         
         h2("Plastic Leakage Risk"),
         
-        selectInput(inputId = "data_type",
-                    label = "Choose data layer:",
-                    choices = c("","DEM", "Water")
-        ),
-        
         plotOutput("histRain", height = 200),
-        
         plotOutput("histWind", height = 200),
     ),
   )
@@ -166,39 +80,130 @@ ui_inter <- navbarPage("Superzip", id="nav",
     hr(),
     
     # display the data in an interactive table
-    DT::dataTableOutput("landfills")
+    DT::dataTableOutput("landfills"),
+    textInput('Long', 'Enter new landfill longitude'),
+    textInput('Lat', 'Enter new landfill latitude'),
+    actionButton("update", "Update Table")
   )
 )
+
+df <- landfills_clusters_sf[-c(2,9:10,12,16:17)]# select relevant columns
+## add long & lat coordinates
+df$long <- st_coordinates(landfills_clusters_sf)[,1]
+df$lat <- st_coordinates(landfills_clusters_sf)[,2]
 
 
 server_inter <- function(input, output, session) {
   
   ## create interactive map with leaflet
   output$map <- renderLeaflet({
-    map
+    map %>%
+      # add toolbox to draw polygons
+      addDrawToolbar(
+        targetGroup = "drawnPoly", 
+        rectangleOptions = F, 
+        polylineOptions = F, 
+        markerOptions = F, 
+        editOptions = editToolbarOptions(selectedPathOptions = selectedPathOptions()), 
+        circleOptions = F,
+        circleMarkerOptions = F,
+        polygonOptions=drawPolygonOptions(showArea=TRUE, repeatMode=F, shapeOptions=drawShapeOptions(fillColor="orange",clickable = TRUE))) %>%
+        addStyleEditor()
   })
   
-  # add new layer to map if "DEM" selected in dropdown
-  observeEvent(input$data_type, {
-    if(input$data_type == "DEM")
-    {
-      leafletProxy("map", session) %>%
-        leafem:::addGeotiff(file = fl, group = "DEM", layerId = "layer1",
-                            colorOptions = leafem:::colorOptions(palette = pal_dem, na.color = "transparent")) %>%
-        addLegend(pal = pal_dem2, values = c(0,80), bins = c(0,80), labFormat = labelFormat(suffix = "°"),
-                  title = "Slope", group = "DEM", layerId = "layer2", position = "bottomleft")
-    }
-  })
+  latlongs <- reactiveValues()   #temporary to hold coords
+  latlongs$df2 <- data.frame(Longitude = numeric(0), Latitude = numeric(0))
   
-  observeEvent(input$data_type, {
-    if(input$data_type == "Water")
-    {
-      leafletProxy("map", session) %>%
-        leafem:::addGeotiff(file = file, group = "Water", layerId = "layer1",
-                            colorOptions = leafem:::colorOptions(palette = pal, na.color = "transparent")) %>%
-        addLegend(pal = pal_jrc, values = c(0,100), bins = c(0,100), labFormat = labelFormat(suffix = "%"),
-                  title = "Water Occurrence", group = "Water", layerId = "layer2", position = "bottomleft")
-    }
+  
+  ## create empty reactive spdf to store drawn polygons
+  value <- reactiveValues()
+  value$drawnPoly <- SpatialPolygonsDataFrame(SpatialPolygons(list()), data = data.frame(notes=character(0), stringsAsFactors = F))
+  
+  # fix the polygon to start another
+  observeEvent(input$map_draw_new_feature, {
+    
+    coor <- unlist(input$map_draw_new_feature$geometry$coordinates)
+    
+    Longitude <- coor[seq(1,length(coor), 2)] 
+    Latitude <- coor[seq(2,length(coor), 2)]
+  
+    isolate(latlongs$df2 <- rbind(latlongs$df2, cbind(Longitude, Latitude)))
+    
+    poly <- Polygon(cbind(latlongs$df2$Longitude, latlongs$df2$Latitude))
+    polys <- Polygons(list(poly), ID=input$map_draw_new_feature$properties$`_leaflet_id`)
+    spPolys <- SpatialPolygons(list(polys))
+    print(spPolys)
+    
+    value$drawnPoly <- rbind(value$drawnPoly, SpatialPolygonsDataFrame(spPolys, data = data.frame(notes=NA, row.names = row.names(spPolys))))
+    
+    ## add polygons to landfills polygons df
+    test <- SpatialPolygonsDataFrame(spPolys, data = data.frame(name=1:length(spPolys), row.names = row.names(spPolys)))
+    test@data$area <- NA
+    test@data$Notes <- NA
+    test@data$location <- NA
+    test@proj4string <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+    #new_polygons <- rbind(landfills_polygons, test)
+    ## export new & old landfills to shapefile
+    #shapefile(x = new_polygons, filename = paste(dir, "/landfills/OpenLandfills_Vietnam.shp", sep = ""), overwrite = T)
+    
+    ## export only new landfills to shapefile, to re-run script only with new landfills (faster)
+    shapefile(x = test, filename = paste(dir, "/landfills/OpenLandfills_Vietnam_new.shp", sep = ""), overwrite = T)
+    
+    ## run DataPreparation script to calculate data of new landfill
+    source("C:/Users/carob/Documents/WWF_PlastikLeakage_Vietnam/PlasticLeakage_DataPreparation.R")
+    
+    
+    ## import outcome of script
+    variables <- readOGR(paste(dir,"/landfill_variables.gpkg", sep= ""))
+    
+    ## predict risk class/cluster of new landfill (without re-running clustering algorithm) ####
+    # basic landfills as training data
+    train <- landfills_clusters@data[,c(7:8,11,13:15)]
+    # newly created landfills (from webapp) as testing data
+    test <- variables@data[,c(7:8,11,13:14)]#[(length(landfills_clusters)+1):length(variables),]
+    knnClust <- class::knn(train = train[,-6], test = test, k = 1, cl = train$km_cluster_unstand)
+    knnClust
+    
+    ## add cluster as row
+    #variables$km_cluster_unstand <- NA
+    variables$km_cluster_unstand <- knnClust #[(length(landfills_clusters)+1):length(variables)] <- knnClust
+    
+    # # drop not needed columns
+    # landfills$risk <- NULL
+    # landfills$risk_label <- NULL
+    # 
+    # # combine all landfills into one spdf
+    # new_variables <- rbind(landfills_clusters[,-c(16:17)], variables[(length(landfills_clusters)+1):length(variables),])
+    # 
+    # ## save results as shapefile
+    # writeOGR(new_variables, paste(dir,"/landfill_clusters.gpkg", sep= ""), driver = "GPKG", overwrite=T)
+    
+    
+    ## update plot upon ending draw
+    observeEvent(input$map_draw_stop, {
+      
+      #replot it - take off the DrawToolbar to clear the features and add it back and use the values from the SPDF to plot the polygons
+      leafletProxy('map') %>% 
+        removeDrawToolbar(clearFeatures=TRUE) %>% removeShape('temp') %>% clearGroup('drawnPoly') %>% 
+        addPolygons(data=value$drawnPoly, popup="poly", group='drawnPoly', color="blue", layerId=row.names(value$drawnPoly)) %>% 
+        addDrawToolbar(
+          targetGroup = "drawnPoly", 
+          rectangleOptions = F, 
+          polylineOptions = F, 
+          markerOptions = F, 
+          editOptions = editToolbarOptions(selectedPathOptions = selectedPathOptions()), 
+          circleOptions=F,
+          polygonOptions=drawPolygonOptions(showArea=TRUE, repeatMode=F, shapeOptions=drawShapeOptions(fillColor="orange",clickable = TRUE)))
+    })
+    
+  latlongs$df2 <- data.frame(Longitude = numeric(0), Latitude = numeric(0)) # clear df
+  
+  #### TODO: plot newly added landfills & risk cluster ####
+  leafletProxy("map", session) %>%
+    #addPolygons(data = landfills_polygons, fill = F, weight = 2, color = "#FFFFCC") %>%
+    addCircleMarkers(data = variables, color = ~cof(km_cluster_unstand), radius = sqrt(variables$area_ha)*2,
+                  fillOpacity = 0.5, label = ~name, group = "Risk")
   })
   
   # create object for clicked marker (=landfill)
@@ -210,17 +215,17 @@ server_inter <- function(input, output, session) {
     leafletProxy("map", session) %>% setView(lng = click$lng, lat = click$lat, zoom = 16)
   })
   
-  # A reactive expression that returns the set of zips that are in map bounds
+  # A reactive expression that returns the set of landfills that are in map bounds (to plot reactive graphs)
   landfillsInBounds <- reactive({
     if (is.null(input$map_bounds))
-      return(landfills_sf[FALSE,])
+      return(landfills_clusters_sf[FALSE,])
     bounds <- input$map_bounds
     latRng <- range(bounds$north, bounds$south)
     lngRng <- range(bounds$east, bounds$west)
 
-    subset(landfills_sf,
-      st_coordinates(landfills_sf)[,2] >= latRng[1] & st_coordinates(landfills_sf)[,2] <= latRng[2] &
-      st_coordinates(landfills_sf)[,1] >= lngRng[1] & st_coordinates(landfills_sf)[,1] <= lngRng[2])
+    subset(landfills_clusters_sf,
+      st_coordinates(landfills_clusters_sf)[,2] >= latRng[1] & st_coordinates(landfills_clusters_sf)[,2] <= latRng[2] &
+      st_coordinates(landfills_clusters_sf)[,1] >= lngRng[1] & st_coordinates(landfills_clusters_sf)[,1] <= lngRng[2])
   })
 
   output$histRain <- renderPlot({
@@ -229,79 +234,34 @@ server_inter <- function(input, output, session) {
       return(NULL)
     hist(landfillsInBounds()$rain,
        main = "Weather Data",
-       xlab = "Heavy Rain Days",
-       xlim = range(landfills_sf$rain),
+       xlab = "Average Precipitation (mm)",
+       xlim = range(landfills_clusters_sf$rain),
        col = '#00ffff',
        border = 'white')
   })
   
   output$histWind <- renderPlot({
-    # If no zipcodes are in view, don't plot
+    # If no landfills are in view, don't plot
     if (nrow(landfillsInBounds()) == 0)
       return(NULL)
     hist(landfillsInBounds()$windspeed,
        main = "",
-       xlab = "Heavy Wind Days",
-       xlim = range(landfills_sf$windspeed),
+       xlab = "Average Wind Speed (km/h)",
+       xlim = range(landfills_clusters_sf$windspeed),
        col = '#00DD00',
        border = 'white')
   })
-
-  ## Data Explorer ##
-  output$landfills <- DT::renderDataTable({
-    df <- landfills_sf %>% 
-      # select relevant columns #### TODO ####
-      select()
+  
+  output$landfills <- DT::renderDT({
+    df
   })
-}
+}  
 
 # Run the app
 shinyApp(ui_inter, server_inter)
 
 
-
-#### TODO: user can input new landfill to data table ####
-# new_landfill <- 
-
-## create spatialpointsdataframe from lat, long coordinates of user input
-new_landfill_spatial <- SpatialPointsDataFrame(coords = c(new_landfill[,c("longitude","latitude")]),
-                                                proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
-                                                data = landfills_sf[0,1:4])
-
-## import current landfill spatial polygons
-landfills <- readOGR(paste(dir, "/landfills/OpenLandfills_Vietnam.shp", sep = ""), use_iconv = T, encoding = "UTF-8")
-
-## add landfill to landfill spatial polygons
-landfills_new <- rbind(landfills, new_landfill_spatial)
-
-## export new landfills to shapefile
-writeOGR(landfills_new, paste(dir, "/landfills/OpenLandfills_Vietnam.shp", sep = ""), driver = "ESRI Shapefile", overwrite = T)
-
-
-
-#### TODO: run DataPreparation script to calculate data of new landfill ####
-source("C:/Users/carob/Documents/WWF_PlastikLeakage_Vietnam/PlasticLeakage_DataPreparation.R")
-
-## import landfill data output of script
-landfill_variables <- readOGR(paste(dir, "/landfill_variables.gpkg", sep = ""))
-landfill_variables_sf <- st_as_sf(landfill_variables)
-
-## only run script for new landfill (faster)
-
-
-## predict risk class/cluster of new landfill (without re-running clustering algorithm) ####
-library(class)
-train <- as.data.frame(landfills_sf[,c(7:8,11:15)][1:20,])
-test <- as.data.frame(landfills_sf[,c(7:8,11:15)][21:31,])
-knnClust <- knn(train = train[,-c(7:8)], test = test[,-c(7:8)], k = 1, cl = train$km_cluster_unstand)
-knnClust
-
-landfill_variables_sf$km_cluster_unstand <- knnClust
-
-## add risk cluster to existing landfill dataframe
-landfills_sf_new <- rbind(landfills_sf, landfill_variables_sf)
-
-## re-run script?
+#### TODO: check if long lat lies in extent of vietnam, before running script ####
 
 
 #### TODO: publish app on shinyapps.io ####
